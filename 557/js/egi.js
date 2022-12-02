174826ed9ca49022ee0262c6b8afaf0a2968e4b5
marcvs <marcvs@users.noreply.github.com>
2022-12-02 13:20:12 +0000

    deploy: a894204f325995fdfe9c7313a7719835e22680fb

--- 557/js/egi.js
+++ 557/js/egi.js
@@ -65,7 +65,6 @@ jQuery(document).ready(function () {
           $('.progress').stop(true, false, true).fadeToggle(100);
       });
   }
-});
 
 jQuery(window).on("load", function () {
   // store this page in session
